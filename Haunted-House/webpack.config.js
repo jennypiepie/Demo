cfb2adb39f182ee634712745e8fd90859e98ddc7
派派 <2416655925@qq.com>
2022-09-03 14:21:15 +0800

    add public directory

--- Haunted-House/webpack.config.js
+++ Haunted-House/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     mode:'development',
     entry: './src',
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
